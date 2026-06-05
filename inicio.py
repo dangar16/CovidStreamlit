08365da369eb50e4f5608271515fa730defbe35f
dangar16 <garcianaranjodaniel@gmail.com>
2026-06-05 12:03:35 +0200

    markdown con html mostrando info inicial

--- inicio.py
+++ inicio.py
@@ -4,3 +4,14 @@ st.set_page_config(
     page_title="COVID-19 España - Dashboard",
     layout="wide", # wide para que el texto ocupe el ancho de la página
 )
+st.markdown("""
+<div>
+  <p style="font-size:2.2rem">COVID-19 España</p>
+  <p>
+    Página web de streamlit para visualizar información sobre la mortalidad por COVID-19 por Comunidad Autónoma, género y período.<br>
+    Datos del INE - Año 2020.
+  </p>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("---")
